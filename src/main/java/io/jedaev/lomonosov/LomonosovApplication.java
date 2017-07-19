37e1b5b045f1af0fe15af359f1c09654c42a5cab
Konstantin Pozhidaev <kos89jed@gmal.com>
2017-07-19 22:54:04 +0300

    init

---
+++ src/main/java/io/jedaev/lomonosov/LomonosovApplication.java
@@ -0,0 +1,12 @@
+package io.jedaev.lomonosov;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LomonosovApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LomonosovApplication.class, args);
+	}
+}
---
+++ src/test/java/io/jedaev/lomonosov/LomonosovApplicationTests.java
@@ -0,0 +1,16 @@
+package io.jedaev.lomonosov;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LomonosovApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
