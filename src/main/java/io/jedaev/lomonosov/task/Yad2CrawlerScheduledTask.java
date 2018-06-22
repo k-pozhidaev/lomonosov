d2369003214b4761c668035dcc7f2202192dcbed
Konstantin Pozhidaev <kos89jed@gmal.com>
2018-06-22 22:41:11 +0300

    Parser development.

--- src/main/java/io/jedaev/lomonosov/task/Yad2CrawlerScheduledTask.java
+++ src/main/java/io/jedaev/lomonosov/task/Yad2CrawlerScheduledTask.java
@@ -1,6 +1,7 @@
 package io.jedaev.lomonosov.task;
 
 import lombok.extern.slf4j.Slf4j;
+import org.jsoup.Jsoup;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.MediaType;
@@ -13,13 +14,12 @@ import org.springframework.web.reactive.function.client.WebClient;
 public class Yad2CrawlerScheduledTask {
     private String url = "http://www.yad2.co.il/Nadlan/rent.php?AreaID=&City=&HomeTypeID=1&fromRooms=4&untilRooms=5.5&fromPrice=&untilPrice=&PriceType=1&FromFloor=&ToFloor=&EnterDate=&Info=";
 
-
-
     @Scheduled(fixedDelay = 5000)
     public void getPagesNum(){
-        log.info("getPagesNum");
+//        log.info("getPagesNum");
     }
 
+//    "/Nadlan/rent.php?AreaID=&City=&HomeTypeID=1&fromRooms=4&untilRooms=5.5&fromPrice=&untilPrice=&PriceType=1&FromFloor=&ToFloor=&EnterDate=&Info="
     @Bean
     CommandLineRunner c(){
         return args -> {
@@ -30,11 +30,17 @@ public class Yad2CrawlerScheduledTask {
                     .accept(MediaType.TEXT_HTML)
                     .retrieve()
                     .bodyToMono(String.class)
-                    .doOnSuccess(log::info)
+                    .doOnSuccess(this::parsePage)
                     .doOnError(e -> log.error("Something goes wrong", e))
-                    .subscribe()
-            ;
+                    .subscribe();
         };
+    }
+
+    private void parsePage(final String htmlPage){
+        log.info(htmlPage);
+        var doc = Jsoup.parse(htmlPage);
+        var el = doc.getElementsByAttributeValueContaining("id", "tr_Ad_");
+        log.info("Elements count {}",  el.size());
     }
 
 }
