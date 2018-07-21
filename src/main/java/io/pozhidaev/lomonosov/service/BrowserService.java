cda9f9f05d71297f8183b5bc37a50da631677707
Konstantin Pozhidaev <kos89jed@gmail.com>
2018-07-21 23:06:04 +0300

    BS Frame.

---
+++ src/main/java/io/pozhidaev/lomonosov/service/BrowserService.java
@@ -0,0 +1,8 @@
+package io.pozhidaev.lomonosov.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class BrowserService {
+    //todo  /Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome --headless --disable-gpu --dump-dom http://www.yad2.co.il/Nadlan/rent.php 1>> a.t 2>> b.t
+}
